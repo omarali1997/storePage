c559c24f55edff5b1797cbd3824f61d75af0faf9
omarali1997 <omar.aziz.9718@gmail.com>
2024-07-28 10:36:10 +0000

    margen

--- app/page.js
+++ app/page.js
@@ -8,14 +8,14 @@ export default function Home() {
      <h1 className="m-auto mt-5 text-[#145DAA] text-[30px] font-sans">اطلب ماء وحاجيات
      </h1>
      <h1 className="m-auto text-[#145DAA] text-[30px] font-sans">نزل التطبيق</h1>
-     <Lnik className="m-auto mt-10" href="https://apps.apple.com/jo/app/kooler/id6526497412" target="_blank">
+     <Lnik className="m-auto mt-5" href="https://apps.apple.com/jo/app/kooler/id6526497412" target="_blank">
      
-     <Image className="m-auto mt-10" src="/App Store.png" alt="Vercel Logo" width={300} height={24} />
+     <Image className="m-auto mt-5" src="/App Store.png" alt="Vercel Logo" width={300} height={24} />
      
      </Lnik> 
 
-      <Lnik className="m-auto mt-10" href="https://play.google.com/store/apps/details?id=com.kooler.water_user&pcampaignid=web_share" target="_blank">
-     <Image className="m-auto mt-10" src="/Google Play.png" alt="Vercel Logo" width={300} height={24} />
+      <Lnik className="m-auto mt-5" href="https://play.google.com/store/apps/details?id=com.kooler.water_user&pcampaignid=web_share" target="_blank">
+     <Image className="m-auto mt-5" src="/Google Play.png" alt="Vercel Logo" width={300} height={24} />
       </Lnik>     
 
     </div>
